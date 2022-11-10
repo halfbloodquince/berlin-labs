e2a4f673bbe7a2a9530babe997a4b9d4fe1f6915
Matt Woods <mattwoodsmaths@gmail.com>
2022-11-10 14:23:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,59 @@
+import { motion } from "framer-motion"
+
+export default function Welcome() {
+
+    const welcomeText = "bespoke data solutions"
+    // const welcomeText = "bespoke.data.solutions( )"
+    // const letters = Array.from(welcomeText)
+    const letters = welcomeText.split(" ")
+
+    const container = {
+        hidden: {opacity:0},
+        visible: {
+            opacity:1, 
+            transition:{ staggerChildren:0.37, delayChildren:1.7}
+        },
+    }
+
+    const child = {
+        visible:{
+            opacity:1,
+            y:0,
+            transition:{type:"spring", damping:12, stiffness:100, ease:"linear"}
+        },
+        hidden:{
+            opacity:0, 
+            y:10,
+            transition:{type:"spring", damping:12, stiffness:100}
+        }
+    }
+
+    return (
+        <div className="welcome">
+            <div className="circle"></div>
+
+            <div className="welcome--icon">
+                <div className="icon--circle"></div>
+                <div className="icon--text--box">
+                    <h2>berlin</h2>
+                    <h2>labs.</h2>
+                </div>
+            </div>
+
+
+            <motion.div className="motion--text--wrapper" variants={container} initial="hidden" animate="visible">
+                {letters.map((letter, index) => (
+                    <motion.div className="motion--text" style={{marginRight:"3px"}} key={index} variants={child}><h2>{letter === " " ? "\u00A0" : letter}</h2></motion.div>
+                ))}
+            </motion.div>
+
+            <div className="welcome--arrow">
+            <svg className="svg--scroll" width="29" height="28" viewBox="0 0 29 28" fill="none" >
+                <path d="M1 14L14.814 27L28 14" />
+                <path d="M1 1L14.814 14L28 1" />
+            </svg>
+
+            </div>    
+        </div>
+    )
+}
